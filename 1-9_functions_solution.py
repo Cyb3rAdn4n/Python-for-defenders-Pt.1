fa4c87fdf811f7c64dc6aabc02ace8ed7c999c2d
Cyb3rAdn4n <134621693+Cyb3rAdn4n@users.noreply.github.com>
2023-07-17 23:50:11 +0500

    Create 1-9_functions_solution.py
    solution to 1-9_functions excercise

---
+++ 1-9_functions_solution.py
@@ -0,0 +1,11 @@
+# Don't delete this line!
+from testme import *
+
+# Write your password validator function
+def valid_password(password: str):
+    
+    return password
+
+valid_password("H3ll070y0U")
+
+testme(valid_password)
